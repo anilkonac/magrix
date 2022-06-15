d472a2afdc774b595a0ec0faade96b960461d532
Anıl Konaç <anil.konac@gmail.com>
2022-06-16 00:38:37 +0300

    Add the bare bones of the game

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/anilkonac/magnix
+
+go 1.18
+
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.3.4
+	golang.org/x/image v0.0.0-20220601225756-64ec528b34cd
+)
+
+require (
+	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20220516021902-eb3e265c7661 // indirect
+	github.com/gofrs/flock v0.8.1 // indirect
+	github.com/jezek/xgb v1.0.1 // indirect
+	golang.org/x/exp/shiny v0.0.0-20220613132600-b0d781184e0d // indirect
+	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
+	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f // indirect
+	golang.org/x/sys v0.0.0-20220614162138-6c1b26c55098 // indirect
+)
---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+	"golang.org/x/image/colornames"
+)
+
+const (
+	screenWidth  = 960
+	screenHeight = 720
+	deltaTime    = 1.0 / 60.0
+)
+
+// Game implements ebiten.Game interface.
+type Game struct{}
+
+// Update proceeds the game state.
+// Update is called every tick (1/60 [s] by default).
+func (g *Game) Update() error {
+	// Write your game's logical update.
+	return nil
+}
+
+// Draw draws the game screen.
+// Draw is called every frame (typically 1/60[s] for 60Hz display).
+func (g *Game) Draw(screen *ebiten.Image) {
+	screen.Fill(colornames.Gray)
+}
+
+// Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
+// If you don't have to adjust the screen size with the outside size, just return a fixed size.
+func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return screenWidth, screenHeight
+}
+
+func main() {
+	game := &Game{}
+	// Specify the window size as you like. Here, a doubled size is specified.
+	ebiten.SetWindowSize(screenWidth, screenHeight)
+	ebiten.SetWindowTitle("Magnix")
+	// Call ebiten.RunGame to start your game loop.
+	if err := ebiten.RunGame(game); err != nil {
+		log.Fatal(err)
+	}
+}
