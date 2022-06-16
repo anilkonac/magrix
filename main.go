500ebfc42857a61810353c500b759c9ab0d7f760
Anıl Konaç <anil.konac@gmail.com>
2022-06-16 13:38:03 +0300

    Draw a prototype player
    Draw a rectangle for prototyping

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"image"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -17,22 +18,41 @@ const (
 	deltaTime    = 1.0 / 60.0
 )
 
-const ratioLandHeight = 1.0 / 4.0
+const (
+	ratioLandHeight = 1.0 / 4.0
+	playerWidth     = 40.0
+	playerHeight    = 100.0
+)
 
 var (
-	landImage       = ebiten.NewImage(1, 1)
-	landDrawOptions = &ebiten.DrawImageOptions{}
+	imageEmpty        = ebiten.NewImage(2, 1)
+	imageLand         *ebiten.Image
+	imagePlayer       *ebiten.Image
+	drawOptionsLand   = &ebiten.DrawImageOptions{}
+	drawOptionsPlayer = &ebiten.DrawImageOptions{}
 )
 
 func init() {
-	landImage.Fill(colornames.Forestgreen)
-	landDrawOptions.GeoM.Scale(screenWidth, screenHeight*ratioLandHeight)
-	landDrawOptions.GeoM.Translate(0, screenHeight*(1.0-ratioLandHeight))
+	imageLand = imageEmpty.SubImage(image.Rect(0, 0, 1, 1)).(*ebiten.Image)
+	imagePlayer = imageEmpty.SubImage(image.Rect(1, 0, 2, 1)).(*ebiten.Image)
+	imageLand.Fill(colornames.Forestgreen)
+	imagePlayer.Fill(colornames.Slategray)
 
+	drawOptionsLand.GeoM.Scale(screenWidth, screenHeight*ratioLandHeight)
+	drawOptionsLand.GeoM.Translate(0, screenHeight*(1.0-ratioLandHeight))
 }
 
 // game implements ebiten.game interface.
-type game struct{}
+type game struct {
+	playerX, playerY float64
+}
+
+func newGame() *game {
+	return &game{
+		playerX: screenWidth / 2.0,
+		playerY: screenHeight / 2.0,
+	}
+}
 
 // Update is called every tick (1/60 [s] by default).
 func (g *game) Update() error {
@@ -45,7 +65,13 @@ func (g *game) Draw(screen *ebiten.Image) {
 	screen.Fill(colornames.Lightskyblue)
 
 	// Draw land
-	screen.DrawImage(landImage, landDrawOptions)
+	screen.DrawImage(imageLand, drawOptionsLand)
+
+	// Draw prototype player
+	drawOptionsPlayer.GeoM.Reset()
+	drawOptionsPlayer.GeoM.Scale(playerWidth, playerHeight)
+	drawOptionsPlayer.GeoM.Translate(g.playerX, g.playerY)
+	screen.DrawImage(imagePlayer, drawOptionsPlayer)
 
 	// Print fps
 	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %.2f", ebiten.CurrentFPS()))
@@ -54,15 +80,17 @@ func (g *game) Draw(screen *ebiten.Image) {
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
 func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
+	// return outsideWidth, outsideHeight
 	return screenWidth, screenHeight
 }
 
 func main() {
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Magrix")
+	// ebiten.SetWindowResizingMode(ebiten.WindowResizingModeEnabled)
+	// ebiten.SetFPSMode(ebiten.FPSModeVsyncOffMaximum)
 
-	game := &game{}
-	if err := ebiten.RunGame(game); err != nil {
+	if err := ebiten.RunGame(newGame()); err != nil {
 		log.Fatal(err)
 	}
 }
