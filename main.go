0e977e75d35778380f55869752715e3120ec2042
Anıl Konaç <anil.konac@gmail.com>
2022-06-16 11:51:48 +0300

    Draw a primitive test environment
    Just a background to test the physics to be added.

--- main.go
+++ main.go
@@ -1,9 +1,13 @@
+// Copyright 2022 Anıl Konaç
+
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"golang.org/x/image/colornames"
 )
 
@@ -13,34 +17,51 @@ const (
 	deltaTime    = 1.0 / 60.0
 )
 
-// Game implements ebiten.Game interface.
-type Game struct{}
+const ratioLandHeight = 1.0 / 4.0
 
-// Update proceeds the game state.
+var (
+	landImage       = ebiten.NewImage(1, 1)
+	landDrawOptions = &ebiten.DrawImageOptions{}
+)
+
+func init() {
+	landImage.Fill(colornames.Forestgreen)
+	landDrawOptions.GeoM.Scale(screenWidth, screenHeight*ratioLandHeight)
+	landDrawOptions.GeoM.Translate(0, screenHeight*(1.0-ratioLandHeight))
+
+}
+
+// game implements ebiten.game interface.
+type game struct{}
+
 // Update is called every tick (1/60 [s] by default).
-func (g *Game) Update() error {
+func (g *game) Update() error {
 	// Write your game's logical update.
 	return nil
 }
 
-// Draw draws the game screen.
 // Draw is called every frame (typically 1/60[s] for 60Hz display).
-func (g *Game) Draw(screen *ebiten.Image) {
-	screen.Fill(colornames.Gray)
+func (g *game) Draw(screen *ebiten.Image) {
+	screen.Fill(colornames.Lightskyblue)
+
+	// Draw land
+	screen.DrawImage(landImage, landDrawOptions)
+
+	// Print fps
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %.2f", ebiten.CurrentFPS()))
 }
 
 // Layout takes the outside size (e.g., the window size) and returns the (logical) screen size.
 // If you don't have to adjust the screen size with the outside size, just return a fixed size.
-func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
+func (g *game) Layout(outsideWidth, outsideHeight int) (int, int) {
 	return screenWidth, screenHeight
 }
 
 func main() {
-	game := &Game{}
-	// Specify the window size as you like. Here, a doubled size is specified.
 	ebiten.SetWindowSize(screenWidth, screenHeight)
 	ebiten.SetWindowTitle("Magrix")
-	// Call ebiten.RunGame to start your game loop.
+
+	game := &game{}
 	if err := ebiten.RunGame(game); err != nil {
 		log.Fatal(err)
 	}
